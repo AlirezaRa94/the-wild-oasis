6a2fa0cccbeb6e6b335c27a73980637622918264
Alireza Raei <alirezaraei17@gmail.com>
2024-11-08 19:21:13 +1100

    feat: add styled-components and global styles
    - Added `styled-components` dependency to `package.json` and `package-lock.json`.
    - Created `GlobalStyles` component in `src/styles/GlobalStyles.js` to define global CSS variables and styles.
    - Updated `App.jsx` to include `GlobalStyles` component.
    - Removed redundant styles from `src/styles/index.css`.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import GlobalStyles from "./styles/GlobalStyles";
+
 function App() {
-  return <div>Hello, World!</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <div>The Wild Oasis!</div>
+    </>
+  );
 }
 
 export default App;
