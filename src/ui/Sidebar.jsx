79d6aaa8f289dcee3b81064765f0a6141571e25e
Alireza Raei <alirezaraei17@gmail.com>
2024-11-09 21:41:28 +1100

    feat: add react-icons and update MainNav with icons
    - Added `react-icons` dependency to `package.json`.
    - Updated `MainNav` component to include icons for navigation links.
    - Enhanced `Sidebar` component styling.

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -8,6 +8,10 @@ const StyledSidebar = styled.aside`
   padding: 3.2rem 2.4rem;
   border-right: 1px solid var(--color-grey-100);
 
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+
   grid-row: 1 / -1;
 `;
 
