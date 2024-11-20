a4236123f3b9182d21d604cd5791a43c51160e14
Alireza Raei <alirezaraei17@gmail.com>
2024-11-20 15:47:53 +1100

    feat: add update settings functionality
    - Created `useUpdateSettings` custom hook to handle updating settings.
    - Integrated `react-hook-form` in `UpdateSettingsForm` for form handling and validation.
    - Added form submission and reset functionality in `UpdateSettingsForm`.

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,8 +1,12 @@
+import { useForm } from "react-hook-form";
+
+import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
-import { useGetSettings } from "./useGetSettings";
 import Spinner from "../../ui/Spinner";
+import { useGetSettings } from "./useGetSettings";
+import { useUpdateSettings } from "./useUpdateSettings";
 
 function UpdateSettingsForm() {
   const {
@@ -14,30 +18,106 @@ function UpdateSettingsForm() {
     } = {},
     isPending,
   } = useGetSettings();
+  const { updateSettings, isUpdating } = useUpdateSettings();
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    values: {
+      minBookingLength,
+      maxBookingLength,
+      maxGuestsPerBooking,
+      breakfastPrice,
+    },
+    disabled: isUpdating,
+  });
+
+  function onSubmit(data) {
+    updateSettings(data);
+  }
 
   if (isPending) return <Spinner />;
 
   return (
-    <Form>
-      <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' defaultValue={minBookingLength} />
-      </FormRow>
-      <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' defaultValue={maxBookingLength} />
-      </FormRow>
-      <FormRow label='Maximum guests/booking'>
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <FormRow
+        label='Minimum nights/booking'
+        error={errors?.minBookingLength?.message}
+      >
         <Input
           type='number'
-          id='max-guests'
-          defaultValue={maxGuestsPerBooking}
+          id='minBookingLength'
+          disabled={isUpdating}
+          {...register("minBookingLength", {
+            required: "This field is required",
+            min: {
+              value: 1,
+              message: "Minimum value is 1",
+            },
+          })}
         />
       </FormRow>
-      <FormRow label='Breakfast price'>
+
+      <FormRow
+        label='Maximum nights/booking'
+        error={errors?.maxBookingLength?.message}
+      >
         <Input
           type='number'
-          id='breakfast-price'
-          defaultValue={breakfastPrice}
+          id='maxBookingLength'
+          disabled={isUpdating}
+          {...register("maxBookingLength", {
+            required: "This field is required",
+            min: {
+              value: 1,
+              message: "Minimum value is 1",
+            },
+          })}
         />
+      </FormRow>
+
+      <FormRow
+        label='Maximum guests/booking'
+        error={errors?.maxGuestsPerBooking?.message}
+      >
+        <Input
+          type='number'
+          id='maxGuestsPerBooking'
+          disabled={isUpdating}
+          {...register("maxGuestsPerBooking", {
+            required: "This field is required",
+            min: {
+              value: 1,
+              message: "Minimum value is 1",
+            },
+          })}
+        />
+      </FormRow>
+
+      <FormRow label='Breakfast price' error={errors?.breakfastPrice?.message}>
+        <Input
+          type='number'
+          id='breakfastPrice'
+          disabled={isUpdating}
+          {...register("breakfastPrice", {
+            required: "This field is required",
+            min: {
+              value: 0,
+              message: "Minimum value is 0",
+            },
+          })}
+        />
+      </FormRow>
+
+      <FormRow>
+        <Button variation='secondary' onClick={() => reset()} type='button'>
+          Reset
+        </Button>
+        <Button disabled={isUpdating} type='submit'>
+          Update Settings
+        </Button>
       </FormRow>
     </Form>
   );
