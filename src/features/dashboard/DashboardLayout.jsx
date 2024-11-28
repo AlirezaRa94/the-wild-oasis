0253da2589631d90d02d0e4f0dcac4253d6ccebd
Alireza Raei <alirezaraei17@gmail.com>
2024-11-28 14:21:38 +1100

    feat: add sales chart to dashboard
    - Created `SalesChart` component to visualize sales data.
    - Integrated `SalesChart` in `DashboardLayout`.
    - Used `recharts` library for rendering the chart.

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 import Spinner from "../../ui/Spinner";
 import Stats from "./Stats";
+import SalesChart from "./SalesChart";
 
 import { useRecentBookings } from "./useRecentBookings";
 import { useRecentStays } from "./useRecentStays";
@@ -36,7 +37,7 @@ function DashboardLayout() {
       />
       <div>Today Activities</div>
       <div>Chart stay durations</div>
-      <div>Chart sales</div>
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
 }
